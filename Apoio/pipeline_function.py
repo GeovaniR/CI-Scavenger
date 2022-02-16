12dc4c11a2d82b8c048503009d58e3c267f16669
Geovani Reolon <geovani.reolon.sousa@gmail.com>
2022-02-16 15:54:43 -0300

    "Alterando nome de algumas funções"

--- Apoio/pipeline_function.py
+++ Apoio/pipeline_function.py
@@ -4,16 +4,15 @@ import jobs_functions as jbs
 import runs_functions as runsf
 from unidecode import unidecode
 
-def test_workflow(i, n, workflows, request_path, username, token, full_repo_path):
+def investigate_workflow_keywords(i, n, workflows, request_path, username, token, full_repo_path):
     n_runs, runs = runsf.runs_path(i, n, workflows, request_path, username, token) # Runs é o arquivo json com os dados das runs e n_runs o número de runs no workflow
     keywords_dict = {"clone":0, "compile":0, "build":0, "ci": 0, "maven":0, "tests":0, "test":0, "regression": 0} # Dicionário de palavras chaves relacionadas a automação de CI/CD
     if (n_runs < n): # Ignora o caso de Pipelines que não são utilizados, mas estão no actions do repo   
         for i in range(0, n_runs): # Faz o loop das funções para o máximo de runs dísponível, mesmo que menor que n
-            keywords_dict = job_keywords_test(i, runs, keywords_dict, full_repo_path, username, token)                   
+            keywords_dict = verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token)                   
     else:   
         for i in range(0, n):
-            keywords_dict = job_keywords_test(i, runs, keywords_dict, full_repo_path, username, token)
-            
+            keywords_dict = verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token)
     return(keywords_dict)
 
 def chr_remove(old, to_remove): # Função que remove caracteres especiais
@@ -22,7 +21,7 @@ def chr_remove(old, to_remove): # Função que remove caracteres especiais
         new_string = new_string.replace(x, '')
     return new_string
 
-def test_name(name, keywords_dict): # Função que testa existência da palavra dentro do dicionário
+def compare_word_with_dict(name, keywords_dict): # Função que analisa a existência da palavra dentro do dicionário
     name = chr_remove(name, "!@#$%¨&*()_-=+'?:<>,.\|^~`}{][;") # Remove caracteres especiais
     name = unidecode(name) # Remove acentos
     lista_palavras = name.split() # lista com as palavras da frase
@@ -31,20 +30,20 @@ def test_name(name, keywords_dict): # Função que testa existência da palavra 
             keywords_dict[palavra] += 1 # Caso encontre soma 1 ocorrência
     return(keywords_dict) # Retorna o dicionário
 
-def test_job_name(j, jobs, keywords_dict): # Função que testa se o nome do Job da evidências de ser para CI/CD
+def verify_jobs_words(j, jobs, keywords_dict): # Função que verifica se o nome do Job da evidências de ser para CI/CD
     job_name = jobs[j].get("name") # Recupera o nome do Job
-    keywords_dict = test_name(job_name, keywords_dict) # Testa o nome
+    keywords_dict = compare_word_with_dict(job_name, keywords_dict) # Testa o nome
     return(keywords_dict) # Retorna o dicionário
 
-def test_steps_name(j, jobs, keywords_dict): # Função que testa se os passos do Job fornecem evidências de CI/CD
+def verify_steps_words(j, jobs, keywords_dict): # Função que verifica se os passos do Job fornecem evidências de CI/CD
     steps = jobs[j].get("steps") # Entra na lista de steps
     n_steps = len(steps) # recupera quantidade de steps
     for k in range(n_steps):
         step_name = steps[k].get("name") # Recupera o nome do Job
-        keywords_dict = test_name(step_name, keywords_dict) # Testa o nome
+        keywords_dict = compare_word_with_dict(step_name, keywords_dict) # Testa o nome
     return(keywords_dict) # Retorna o dicionário
 
-def job_keywords_test(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
+def verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
     id_jobs = runs[i].get("id") # recupera id da run
     condition = runs[i].get("conclusion") 
     if (condition != "action_required"): # Verifica se a run não tem acesso privado
@@ -54,7 +53,7 @@ def job_keywords_test(i, runs, keywords_dict, full_repo_path, username, token): 
         n_jobs = int(jobs_json["total_count"]) # Recupera a quantidade total de jobs
         jobs = jobs_json["jobs"] # Entra nas informações sobre os jobs
         for j in range(n_jobs): # Percore cada Job existente
-            keywords_dict = test_job_name(j, jobs, keywords_dict) # Testa o nome do job
-            keywords_dict = test_steps_name(j, jobs, keywords_dict) # Testa os passos do job
+            keywords_dict = verify_jobs_words(j, jobs, keywords_dict) # Testa o nome do job
+            keywords_dict = verify_steps_words(j, jobs, keywords_dict) # Testa os passos do job
     return(keywords_dict)
 
