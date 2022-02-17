191179e497707f0fe01235b31a8bb831a95e183d
Geovani Reolon <geovani.reolon.sousa@gmail.com>
2022-02-17 10:04:59 -0300

    "Mudando análise das palavras nos jobs apenas para última run disponível"

--- Apoio/pipeline_function.py
+++ Apoio/pipeline_function.py
@@ -7,12 +7,7 @@ from unidecode import unidecode
 def investigate_workflow_keywords(i, n, workflows, request_path, username, token, full_repo_path):
     n_runs, runs = runsf.runs_path(i, n, workflows, request_path, username, token) # Runs é o arquivo json com os dados das runs e n_runs o número de runs no workflow
     keywords_dict = {"clone":0, "compile":0, "build":0, "ci": 0, "maven":0, "tests":0, "test":0, "regression": 0} # Dicionário de palavras chaves relacionadas a automação de CI/CD
-    if (n_runs < n): # Ignora o caso de Pipelines que não são utilizados, mas estão no actions do repo   
-        for i in range(0, n_runs): # Faz o loop das funções para o máximo de runs dísponível, mesmo que menor que n
-            keywords_dict = verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token)                   
-    else:   
-        for i in range(0, n):
-            keywords_dict = verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token)
+    keywords_dict = verify_words_in_run(i, runs, keywords_dict, full_repo_path, username, token)                   
     return(keywords_dict)
 
 def chr_remove(old, to_remove): # Função que remove caracteres especiais
@@ -43,17 +38,19 @@ def verify_steps_words(j, jobs, keywords_dict): # Função que verifica se os pa
         keywords_dict = compare_word_with_dict(step_name, keywords_dict) # Testa o nome
     return(keywords_dict) # Retorna o dicionário
 
-def verify_words_run_to_run(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
-    id_jobs = runs[i].get("id") # recupera id da run
+def verify_words_in_run(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
     condition = runs[i].get("conclusion") 
-    if (condition != "action_required"): # Verifica se a run não tem acesso privado
-        path_jobs = full_repo_path + "/actions/runs/{0}/jobs".format(id_jobs) # define caminho para os jobs da run
-        res_jobs = requests.get(path_jobs, auth= (username, token)) # Executa chamada para informações dos jobs
-        jobs_json = res_jobs.json() # Transformar informações para formato json
-        n_jobs = int(jobs_json["total_count"]) # Recupera a quantidade total de jobs
-        jobs = jobs_json["jobs"] # Entra nas informações sobre os jobs
-        for j in range(n_jobs): # Percore cada Job existente
-            keywords_dict = verify_jobs_words(j, jobs, keywords_dict) # Testa o nome do job
-            keywords_dict = verify_steps_words(j, jobs, keywords_dict) # Testa os passos do job
+    while (condition == "action_required"): # Loop até achar uma run sem acesso privado
+        i += 1
+        condition = runs[i].get("conclusion")
+    id_jobs = runs[i].get("id") # recupera id da run
+    path_jobs = full_repo_path + "/actions/runs/{0}/jobs".format(id_jobs) # define caminho para os jobs da run
+    res_jobs = requests.get(path_jobs, auth= (username, token)) # Executa chamada para informações dos jobs
+    jobs_json = res_jobs.json() # Transformar informações para formato json
+    n_jobs = int(jobs_json["total_count"]) # Recupera a quantidade total de jobs
+    jobs = jobs_json["jobs"] # Entra nas informações sobre os jobs
+    for j in range(n_jobs): # Percore cada Job existente
+        keywords_dict = verify_jobs_words(j, jobs, keywords_dict) # Testa o nome do job
+        keywords_dict = verify_steps_words(j, jobs, keywords_dict) # Testa os passos do job
     return(keywords_dict)
 
