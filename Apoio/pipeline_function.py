66bac6c0c305bc5a595f24748fc4f60e8636cd9b
Geovani Reolon <geovani.reolon.sousa@gmail.com>
2022-02-17 11:14:53 -0300

    Adicionando alterações da validação dos workflows

--- Apoio/pipeline_function.py
+++ Apoio/pipeline_function.py
@@ -1,14 +1,18 @@
 import requests
-import print_functions as prt
-import jobs_functions as jbs
 import runs_functions as runsf
 from unidecode import unidecode
 
 def investigate_workflow_keywords(i, n, workflows, request_path, username, token, full_repo_path):
     n_runs, runs = runsf.runs_path(i, n, workflows, request_path, username, token) # Runs é o arquivo json com os dados das runs e n_runs o número de runs no workflow
     keywords_dict = {"clone":0, "compile":0, "build":0, "ci": 0, "maven":0, "tests":0, "test":0, "regression": 0} # Dicionário de palavras chaves relacionadas a automação de CI/CD
-    keywords_dict = verify_words_in_run(i, runs, keywords_dict, full_repo_path, username, token)                   
-    return(keywords_dict)
+    keywords_dict, words_found = verify_workflow_words(i, workflows, keywords_dict)
+    if (words_found > 0 ): # Testa se encontrou alguma palavra no nome do worflow
+        return(keywords_dict, True) # Caso tenha encontrado já retorna que o workflow pode ser testado
+    else:    # Caso contrário testa os jobs
+        keywords_dict, words_found = verify_jobs_and_steps(0, runs, keywords_dict, full_repo_path, username, token)
+        if (words_found > 0 ): # Testa se encontrou alguma palavra no nome do job e passos
+            return(keywords_dict, True) # Caso tenha encontrado já retorna que o workflow pode ser testado                  
+    return(keywords_dict, False)
 
 def chr_remove(old, to_remove): # Função que remove caracteres especiais
     new_string = old.lower()
@@ -17,32 +21,39 @@ def chr_remove(old, to_remove): # Função que remove caracteres especiais
     return new_string
 
 def compare_word_with_dict(name, keywords_dict): # Função que analisa a existência da palavra dentro do dicionário
+    words_found = 0 # Variável que armazena o número de palavras encontradas
     name = chr_remove(name, "!@#$%¨&*()_-=+'?:<>,.\|^~`}{][;") # Remove caracteres especiais
     name = unidecode(name) # Remove acentos
     lista_palavras = name.split() # lista com as palavras da frase
     for palavra in lista_palavras: # testa de palavra em palavra com o dicionário
         if (palavra in keywords_dict):
             keywords_dict[palavra] += 1 # Caso encontre soma 1 ocorrência
-    return(keywords_dict) # Retorna o dicionário
+            words_found += 1 # Soma uma palavra encontrada          
+    return(keywords_dict, words_found) # Retorna o dicionário
+
+def verify_workflow_words(i, workflows, keywords_dict): # Função que verifica se o nome do workflow da evidências de ser para CI/CD
+    workflow_name = workflows[i].get("name") # Recupera o nome do workflow
+    keywords_dict, words_found = compare_word_with_dict(workflow_name, keywords_dict) # Testa o nome do workflow
+    return(keywords_dict, words_found) # Retorna o dicionário e o número de palavras encontradas
 
 def verify_jobs_words(j, jobs, keywords_dict): # Função que verifica se o nome do Job da evidências de ser para CI/CD
     job_name = jobs[j].get("name") # Recupera o nome do Job
-    keywords_dict = compare_word_with_dict(job_name, keywords_dict) # Testa o nome
-    return(keywords_dict) # Retorna o dicionário
+    keywords_dict, words_found = compare_word_with_dict(job_name, keywords_dict) # Testa o nome
+    return(keywords_dict, words_found) # Retorna o dicionário e o número de palavras encontradas
 
 def verify_steps_words(j, jobs, keywords_dict): # Função que verifica se os passos do Job fornecem evidências de CI/CD
     steps = jobs[j].get("steps") # Entra na lista de steps
     n_steps = len(steps) # recupera quantidade de steps
     for k in range(n_steps):
         step_name = steps[k].get("name") # Recupera o nome do Job
-        keywords_dict = compare_word_with_dict(step_name, keywords_dict) # Testa o nome
-    return(keywords_dict) # Retorna o dicionário
+        keywords_dict, words_found = compare_word_with_dict(step_name, keywords_dict) # Testa o nome
+    return(keywords_dict, words_found) # Retorna o dicionário e o número de palavras encontradas
 
-def verify_words_in_run(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
+def verify_jobs_and_steps(i, runs, keywords_dict, full_repo_path, username, token): # i percore o número de runs
     condition = runs[i].get("conclusion") 
     while (condition == "action_required"): # Loop até achar uma run sem acesso privado
         i += 1
-        condition = runs[i].get("conclusion")
+        condition = runs[i].get("conclusion")   
     id_jobs = runs[i].get("id") # recupera id da run
     path_jobs = full_repo_path + "/actions/runs/{0}/jobs".format(id_jobs) # define caminho para os jobs da run
     res_jobs = requests.get(path_jobs, auth= (username, token)) # Executa chamada para informações dos jobs
@@ -50,7 +61,12 @@ def verify_words_in_run(i, runs, keywords_dict, full_repo_path, username, token)
     n_jobs = int(jobs_json["total_count"]) # Recupera a quantidade total de jobs
     jobs = jobs_json["jobs"] # Entra nas informações sobre os jobs
     for j in range(n_jobs): # Percore cada Job existente
-        keywords_dict = verify_jobs_words(j, jobs, keywords_dict) # Testa o nome do job
-        keywords_dict = verify_steps_words(j, jobs, keywords_dict) # Testa os passos do job
-    return(keywords_dict)
+        keywords_dict, words_found = verify_jobs_words(j, jobs, keywords_dict) # Testa o nome do job
+        if (words_found > 0):
+             return(keywords_dict, words_found) # Retorna o dicionário e o número de palavras encontradas 
+    for j in range(n_jobs): # Percore cada Job existente
+        keywords_dict, words_found = verify_steps_words(j, jobs, keywords_dict) # Testa os passos do job
+        if (words_found > 0):
+             return(keywords_dict, words_found) # Retorna o dicionário e o número de palavras encontradas                       
+    return(keywords_dict, words_found)
 
--- Principal/Main_code_input.py
+++ Principal/Main_code_input.py
@@ -20,13 +20,14 @@ def main():
     repo_path, full_repo_path, request_path, workflows, n_pipelines = work.define_workflow_path(username, token, owner, repo, verbose)
     json_data = {repo_path:[{"n_worfklows" : n_pipelines}]}
     for i in range(0, n_pipelines): ## Loop para que seja rodada as funções em cada pipeline
-        workflow_name, workflow_state = work.workflow_name_state(i, workflows, verbose)
-        temp_start, temp_close, diff_temp = work.calculate_development_time(i, workflows, verbose)
-        keywords_dict = pipeline.test_workflow(i, n, workflows, request_path, username, token, full_repo_path)
-        prt.my_print(keywords_dict, verbose)
-        perc_sucess, perc_branch_main, perc_branch_outros, runs_time_list, n_jobs_list, n_runs, n_runs_analyzed, runs_time_dict, runs_diff_time = calc.calculate_runs(i, n, workflows, username, token, request_path, full_repo_path, verbose)
-        workflow_json = js.json_transform(workflow_name, workflow_state, temp_start, temp_close, diff_temp, perc_sucess, perc_branch_main, perc_branch_outros, runs_time_list, n_jobs_list, n_runs, n_runs_analyzed, runs_time_dict, runs_diff_time)
-        json_data[repo_path].append(workflow_json)        
+        keywords_dict, validate_worflow = pipeline.investigate_workflow_keywords(i, n, workflows, request_path, username, token, full_repo_path)
+        if (validate_worflow):
+            workflow_name, workflow_state = work.workflow_name_state(i, workflows, verbose)
+            temp_start, temp_close, diff_temp = work.calculate_development_time(i, workflows, verbose)
+            prt.my_print(keywords_dict, verbose)
+            perc_sucess, perc_branch_main, perc_branch_outros, runs_time_list, n_jobs_list, n_runs, n_runs_analyzed, runs_time_dict, runs_diff_time = calc.calculate_runs(i, n, workflows, username, token, request_path, full_repo_path, verbose)
+            workflow_json = js.json_transform(workflow_name, workflow_state, temp_start, temp_close, diff_temp, perc_sucess, perc_branch_main, perc_branch_outros, runs_time_list, n_jobs_list, n_runs, n_runs_analyzed, runs_time_dict, runs_diff_time)
+            json_data[repo_path].append(workflow_json)        
     isValid = js.validateJSON(json_data) # Valida Texto que será transformado em Json
     js.build_json_file(isValid, name_json, json_data) # Função que cria arquivo .json
     prt.my_print(json_data, verbose) # Printa saídas caso desejado
